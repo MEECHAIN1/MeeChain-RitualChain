c1ef1ae98485680bddefeba17212d06fc6c70218
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-12 02:02:20 +0700

    Create MeeBot component with initial messages

---
+++ src/components/MeeBot.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+const MeeBot: React.FC = () => {
+  const [messages, setMessages] = useState<string[]>([
+    'สวัสดีจาก MeeBot 👋',
+    'I can help you stake and perform rituals.'
+  ])
+
+  useEffect(() => {
+    // placeholder: in future we can poll for events or chat messages
+  }, [])
+
+  return (
+    <aside style={{ border: '1px solid #eee', padding: 12 }}>
+      <h3>MeeBot</h3>
+      <ul>
+        {messages.map((m, i) => <li key={i}>{m}</li>)}
+      </ul>
+    </aside>
+  )
+}
+
+export default MeeBot
