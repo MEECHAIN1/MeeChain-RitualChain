1a44787c2a745844bf35f0991c01a27bb04c57c7
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-16 18:37:03 +0700

    Updated client.ts

--- src/wagmi/client.ts
+++ src/wagmi/client.ts
@@ -39,7 +39,6 @@ export const wagmiClient = createConfig({
       chains,
       options: {
         projectId:"c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
-        // showQrModal: true is default for wagmi walletconnect connector v2 integrations
       },
     }),
   ],
